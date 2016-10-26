ad990f40436b4da8ff2cef4f77d8d1f6f5c0b977
Bryan Boreham <bjboreham@gmail.com>
2016-10-26 18:21:29 +0100

    Don't try so hard to clean up kubelet

--- test/config.sh
+++ test/config.sh
@@ -275,7 +275,6 @@ start_suite() {
         PLUGIN_FILTER=$(docker_on $host inspect -f 'grep -v {{printf "%.12s" .Id}}' weaveplugin 2>/dev/null) || PLUGIN_FILTER=cat
         rm_containers $host $(docker_on $host ps -aq 2>/dev/null | $PLUGIN_FILTER)
         weave_on $host reset 2>/dev/null
-        run_on $host "sudo find /var/lib/kubelet | xargs -n 1 findmnt -n -t tmpfs -o TARGET -T | uniq | xargs -r sudo umount -v"
         run_on $host "sudo rm -r -f /etc/kubernetes /var/lib/kubelet /var/lib/etcd"
     done
     whitely echo "$@"
