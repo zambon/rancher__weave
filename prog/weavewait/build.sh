a3f1a7cca33cd4938821a9d208d8a0c7744319d8
Henrique Zambon <henrique.zambon@gmail.com>
2017-08-28 16:07:36 -0400

    Fix case

--- prog/weavewait/build.sh
+++ prog/weavewait/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-GOARCH=amd64 go build -ldflags "-X github.com/weaveworks/weave/net.VethName=eth0 -linkmode external -extldflags -static -s" -tags iface -o r-linux-amd64
-GOARCH=ppc64le go build -ldflags "-X github.com/weaveworks/weave/net.VethName=eth0 -linkmode external -extldflags -static -s" -tags iface -o r-linux-ppc64le
+GOARCH=amd64 go build -ldflags "-X github.com/weaveworks/weave/net.VethName=eth0 -linkmode external -extldflags -static -s" -tags iface -o r-Linux-amd64
+GOARCH=ppc64le go build -ldflags "-X github.com/weaveworks/weave/net.VethName=eth0 -linkmode external -extldflags -static -s" -tags iface -o r-Linux-ppc64le
