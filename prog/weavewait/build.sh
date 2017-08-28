dab6679717c80068bced02e38a618e4d2d42aa8c
Henrique Zambon <henrique.zambon@gmail.com>
2017-08-28 15:46:48 -0400

    Multi-arch build

--- prog/weavewait/build.sh
+++ prog/weavewait/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-go build -ldflags "-X github.com/weaveworks/weave/net.VethName=eth0 -linkmode external -extldflags -static -s" -tags iface -o r
-
+GOARCH=amd64 go build -ldflags "-X github.com/weaveworks/weave/net.VethName=eth0 -linkmode external -extldflags -static -s" -tags iface -o r-linux-amd64
+GOARCH=ppc64le go build -ldflags "-X github.com/weaveworks/weave/net.VethName=eth0 -linkmode external -extldflags -static -s" -tags iface -o r-linux-ppc64le
