a0fa52ed2c747512c9de1d864a65b70d9310a38d
Henrique Zambon <henrique.zambon@gmail.com>
2017-10-22 14:20:44 -0400

    Add OS and arch to artifact

--- prog/weavewait/build.sh
+++ prog/weavewait/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-go build -ldflags "-X github.com/weaveworks/weave/net.VethName=eth0 -linkmode external -extldflags -static -s" -tags iface -o r
+OS="$(uname -s)"
+ARCH="$(uname -m)"
 
+go build -ldflags "-X github.com/weaveworks/weave/net.VethName=eth0 -linkmode external -extldflags -static -s" -tags iface -o "r-$OS-$ARCH"
